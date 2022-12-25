2ae0308353fb92d13a5fe49e264a1f8154ca0fd3
FeoktistoffRoman <acsalotl@gmail.com>
2022-12-26 02:38:39 +0300

    Готовая работа

---
+++ JS/valid.js
@@ -0,0 +1,22 @@
+function myFunc() {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+}
+
+myFunc();
